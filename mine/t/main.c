8f4809ccd87f207b76787a88f9f79db1d8cc1f9b
root <mfjdsg@gmail.com>
2021-09-17 08:17:40 +0000

    init

---
+++ mine/t/main.c
@@ -0,0 +1,23 @@
+struct test1
+{
+    struct test2
+    {
+        int a;
+        /* data */
+    }a1;
+    
+    /* data */
+};
+struct test1 t = {
+    .a1{
+        .a
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    
+
+    return 0;
+}
